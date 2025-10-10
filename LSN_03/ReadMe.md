d58ef6e86be62a300c69e06e1f9ec32058888cf9
Tommaso Canclini <tommasocanclini1999@gmail.com>
2025-10-10 17:19:23 +0200

    First upload LSN_03

---
+++ LSN_03/ReadMe.md
@@ -0,0 +1,71 @@
+# Exercise 3: European option price simulation
+
+This document provides the necessary guidelines for compiling, executing, and analyzing the C++ codes for the third assignment of the Computational Physics Laboratory (LSN).
+
+---
+
+## 0. Introduction
+
+This exercise is dedicated to the simulation of financial asset price, modeled by a **Geometric Brownian Motion (GBM)**, in order to compute the estimated value of **European call and put options**. These values are fondamental for structuring option contracts. The results produced through Monte Carlo simulations are **statistically compared** to the analytical values coming from the **Black-Scholes-Merton (BSM) model**.
+
+---
+
+## 1. Project Structure and Data Flow
+
+The repository root contains the analysis environment and the exercise folder (`es_3`).
+
+### Detailed Directory Tree:
+
+```
+LSN_03/
+├── LSN_Exercises_03.ipynb          # Jupyter Notebook for consolidated analysis.
+├── LSN_Notebook_03.ipynb
+├── ReadMe.md
+└── es_3                            # Exercise 3 directory
+    ├── OUTPUT                      # Repository for raw statistical data (`*.data`).
+    ├── main.cpp                    # Primary C++ source code.
+    ├── main.exe                    # Executable binary.
+    ├── main.o                      # Intermediate object file.
+    ├── makefile                    # Build automation script.
+    └── plot                        # Repository for graphical output
+```
+
+
+### Execution Workflow:
+
+The analysis notebook (`LSN_Notebook_01.ipynb`) processes data from `es_3/OUTPUT/` directory. Therefore, **all simulations must be executed** before proceeding to the final analysis step.
+
+---
+
+## 2. Maintenance and Cleanup Commands
+
+These commands are available inside (`es_3`) directory for granular file management.
+
+| Command | Function |
+| :--- | :--- |
+| `make remove_exe` | Removes the executable file (`main.exe`). |
+| `make remove_o` | Removes all object files (`*.o`). |
+| `make remove_output` | **CAUTION**: Removes all data files (`*.data`) from the local `OUTPUT/` folder. |
+| `make remove_plot` | Removes all plot during the notebook analysis |
+
+## 3. Operational Guide
+
+All commands in this guide assume you are starting from the **exercise directory** (`LSN_EXERCISES/LSN_03/es_3`).
+
+### Step 1: Cleanup, compilation and execution
+
+The following commands ensure a **clean build** and **execution**. They first remove old build files (`main.o`, `main.exe`, `*.data`, `*.png` files) and then compile and run.
+
+```bash
+make remove_o remove_exe remove_output remove_plot
+make
+./main.exe
+```
+
+### Step 2: Launch the Analysis Notebook
+
+After all data files are successfully generated in the `OUTPUT/`, launch the analysis notebook:
+
+```bash
+cd .. && jupyter-notebook LSN_Notebook_03.ipynb
+```
---
+++ LSN_03/es_3/main.cpp
@@ -0,0 +1,205 @@
+#include <cmath>
+#include <fmt/format.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+#include "../../generator/random.h"
+#include "../../functions.h"
+
+using namespace std;
+
+int main(){
+
+    // generator setting and start
+    Random rnd ;
+    rnd.StartRandom();
+    // random generator to print asset price time evolution
+    Random rnd_print ;
+    rnd_print.StartRandom(1);
+    
+    // lambda function used
+    auto asset_evolution = [&] (double S0, double drift, double volatility, double t, double z){
+        return S0 * exp((drift - 0.5 * volatility * volatility) * t + volatility * z * sqrt(t));
+    };
+
+    auto call_prize = [] (double S, double K){
+        return (S - K > 0 ? S - K : 0);
+    };
+
+    auto put_prize = [] (double S, double K){
+        return (K - S > 0 ? K - S : 0);
+    };
+
+    // throws number
+    int M{1000000};
+    // blocks numbers
+    int N{100};
+    // throws in each block
+    int L{M/N};
+    // start asset price
+    double S0{100};
+    // expiry time
+    double T{1};
+    // strike price
+    double K{100};
+    // risk-free interest rate
+    double drift{0.1};
+    // price volatility
+    double volatility{0.25};
+    // discount factor
+    double discount_factor{exp(-drift * T)};
+
+    // accumulating variables to perform data-blocking
+    double acc_call{};
+    double call_block_mean{};
+    double call_block_mean2{};
+    double call_mean_acc{};
+    double call_mean2_acc{};
+
+    double acc_put{};
+    double put_block_mean{};
+    double put_block_mean2{};
+    double put_mean_acc{};
+    double put_mean2_acc{};
+    double S{};
+    
+    ofstream call_out("OUTPUT/call.data");
+    ofstream put_out("OUTPUT/put.data");
+
+    // ex 3.1.1
+
+    call_out << "# BLOCK:          CURR_MEAN:          PROG_MEAN:             ERROR:" << endl;
+    put_out << "# BLOCK:          CURR_MEAN:          PROG_MEAN:             ERROR:" << endl;
+
+    for(int i{}; i < N; i++){
+
+        acc_call = 0;
+        acc_put = 0;
+
+        for(int j{}; j < L; j++){
+            S = asset_evolution(S0, drift, volatility, T, rnd.Gauss(0.0, 1.0));
+            acc_call += call_prize(S, K);
+            acc_put += put_prize(S, K);
+            // exponential de-evolution to discont asset price at t = 0
+            // acc_call += discount_factor * call_prize(S, K);
+            // acc_put += discount_factor * put_prize(S, K);
+        }
+
+        // call_block_mean = acc_call / L;
+        // put_block_mean = acc_put / L;
+        // exponential de-evolution to discont asset price at t = 0
+        call_block_mean = discount_factor * acc_call / L;
+        put_block_mean = discount_factor * acc_put / L;
+        call_block_mean2 = call_block_mean * call_block_mean;
+        put_block_mean2 = put_block_mean * put_block_mean;
+        
+        call_mean_acc += call_block_mean;
+        call_mean2_acc += call_block_mean2;
+        put_mean_acc += put_block_mean;
+        put_mean2_acc += put_block_mean2;
+
+        call_out << setw(8) << i
+                 << setw(20) << call_block_mean
+                 << setw(20) << call_mean_acc/(i+1) 
+                 << setw(20) << error(call_mean_acc, call_mean2_acc, i) 
+                 << endl;
+                  
+        put_out << setw(8) << i
+                << setw(20) << put_block_mean
+                << setw(20) << put_mean_acc/(i+1) 
+                << setw(20) << error(put_mean_acc, put_mean2_acc, i) 
+                << endl;
+
+    }
+
+    call_out.close();
+    put_out.close();
+    
+    // ex 3.1.2
+    int nstep{100};
+    double time_step{double(T)/double(nstep)}; 
+    acc_call = 0;
+    call_block_mean = 0;
+    call_block_mean2 = 0;
+    call_mean_acc = 0;
+    call_mean2_acc = 0;
+
+    acc_put = 0;
+    put_block_mean = 0;
+    put_block_mean2 = 0;
+    put_mean_acc = 0;
+    put_mean2_acc = 0;
+    S = 0;
+    double old_S{};
+
+    ofstream call_out2("OUTPUT/call2.data");
+    ofstream put_out2("OUTPUT/put2.data");
+    ofstream asset_out2("OUTPUT/asset2.data");
+
+    call_out2 << "# BLOCK:          CURR_MEAN:          PROG_MEAN:             ERROR:" << endl;
+    put_out2 << "# BLOCK:          CURR_MEAN:          PROG_MEAN:             ERROR:" << endl;
+    asset_out2 << "# TIME:         SPOT_PRICE:" << endl;
+    
+    int print_1{int(rnd_print.Rannyu(0, L))};
+    int print_2{int(rnd_print.Rannyu(0, N))};
+
+    for(int i{}; i < N; i++){
+        
+        acc_call = 0;
+        acc_put = 0;
+
+        for(int j{}; j < L; j++){
+            old_S = S0;
+            for(int k{}; k < 100; k++){
+                // discrete time evolution
+                S = asset_evolution(old_S, drift, volatility, time_step, rnd.Gauss(0.0, 1.0));
+                // setting old price to current price 
+                old_S = S;
+                // printing price evolution of 1 random simulation
+                if(j == print_1 && i == print_2){
+                    asset_out2 << setw(7) << k
+                               << setw(20) << S 
+                               << endl;
+                }
+            }
+            acc_call += call_prize(S, K);
+            acc_put += put_prize(S, K);
+            // exponential de-evolution to discont asset price at t = 0
+            // acc_call += discount_factor * call_prize(S, K);
+            // acc_put += discount_factor * put_prize(S, K);
+        }
+        // call_block_mean = acc_call / L;
+        // put_block_mean = acc_put / L;
+        // exponential de-evolution to discont asset price at t = 0
+        call_block_mean = discount_factor * acc_call / L;
+        put_block_mean = discount_factor * acc_put / L;
+        call_block_mean2 = call_block_mean * call_block_mean;
+        put_block_mean2 = put_block_mean * put_block_mean;
+        
+        call_mean_acc += call_block_mean;
+        call_mean2_acc += call_block_mean2;
+        put_mean_acc += put_block_mean;
+        put_mean2_acc += put_block_mean2;
+
+        call_out2 << setw(8) << i
+                  << setw(20) << call_block_mean
+                  << setw(20) << call_mean_acc/(i+1) 
+                  << setw(20) << error(call_mean_acc, call_mean2_acc, i) 
+                  << endl;
+
+        put_out2 << setw(8) << i
+                  << setw(20) << put_block_mean
+                  << setw(20) << put_mean_acc/(i+1) 
+                  << setw(20) << error(put_mean_acc, put_mean2_acc, i) 
+                  << endl;
+
+    }
+
+    call_out2.close();
+    put_out2.close();
+    asset_out2.close();
+
+    return 0;
+
+}
